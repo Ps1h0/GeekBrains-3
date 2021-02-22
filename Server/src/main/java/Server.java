5d6708de050d9cfeee0813fa264d9691f28fe8f5
Ps1h0 <platonov.liceum@mail.ru>
2021-02-22 21:36:02 +0800

    Исправления добавлены

--- Server/src/main/java/Server.java
+++ Server/src/main/java/Server.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.SQLException;
 import java.time.LocalTime;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
